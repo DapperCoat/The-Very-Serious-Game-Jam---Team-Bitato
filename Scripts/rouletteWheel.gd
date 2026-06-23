5a2c8b7eb5799e8d4efa0cfc9682ffb85c5d7f0a
spi231 <maxkranc08@gmail.com>
2026-06-22 23:38:22 -0400

    The spinner is fully working. It takes the user's mouse drag, spin's the wheel accordingly, and dynamically slows it down

--- Scripts/rouletteWheel.gd
+++ Scripts/rouletteWheel.gd
@@ -1,50 +1,72 @@
 extends Node2D
 
-
-# placeholder to see if the function works
-func _process(delta):
-	if wheelSpinActive and Input.is_action_just_pressed("ui_accept"):
-		wheelSpin()
-
-
-# ok, so we're going to track the mouse pos while it's in the area raidus
-# the wheel will spin with the mouse from it's original position
-# then when it leaves, we find the arc distance
-# and use that for spin power
-# this is about to be the least efficent code you've ever seen
+# REMEMBER, spinActive SHOULD ONLY BE TRUE WHEN THE PLAYER CAN SPIN THE WHEEL!!
+var spinActive := true ## To allow the system to know when the player should be allowed to drag the wheel
+var flyActive := false ## To allow the system to know when the wheel should start spinning on its own
+var hasSpinBeenActivated := false ## bug fixing reasons
 var mouseInArea: bool ## Detects whether mouse is in or our of the roulette's radius
-var outOfTime : bool ## if you run out of time to drag the wheel
-@export var sprite := Node2D ## to make it easier to change the sprite nodes without fucking up the code
-var wheelSpinActive := true ## placeholder variable
-var counter: int
-func wheelSpin():
-	wheelSpinActive = false
-	outOfTime = false
-	$"Mouse Grab Time Limit".start()
-	var initialMouseX = get_local_mouse_position().x
-	var initialMouseY = get_local_mouse_position().y
-	counter = 0 
-	while mouseInArea and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT): #and not outOfTime:
-		# we're doing math now. cool. love math
-		# my calculus exam is tomorrow and I'm doing this instead
-		# ok watch this
-		# we do the dot product with components
-		# and then undo it with vectors
-		# and get the angle
-		# this took like an hour to figure out
-		var firstLine = sqrt(initialMouseX**2 + initialMouseY**2) ## this is the line between the origin and the initial mouse pos
-		var currentLine = sqrt(get_local_mouse_position().x**2 + get_local_mouse_position().y**2) ## this is the line between the current mouse pos and the origin
-		# firstLine*currentLine*cos(theta) = initialMouseX*get_local_mouse_position().x + initialMouseY*get_local_mouse_position().y
-		# cos(theta) = (initialMouseX*get_local_mouse_position().x + initialMouseY*get_local_mouse_position().y) / firstLine*currentLine
-		var theta = acos((initialMouseX*get_local_mouse_position().x + initialMouseY*get_local_mouse_position().y) / firstLine*currentLine)
-		if get_local_mouse_position().x != 0 and get_local_mouse_position().y != 0:
-			sprite.rotation += theta
-		# the following code is to make sure it stops crashing during testing
-		counter += 1
-		if counter > 50000:
-			print(theta)
-			break
-	wheelSpinActive = true
+@onready var grabTimer := $"Mouse Grab Time Limit" ## so that if you change anything about the timer node, it's easier to change without changing every reference to it
+@onready var sprite := $"Placeholder Sprite"## so that if you change anything about the sprite node, it's easier to change without changing every reference to it
+var outOfTime: bool  ## if you run out of time to drag the wheel
+var firstClick := true ## is this the first click or not
+var initialMousePos: Vector2 ## get the first mouse pos on click to find the wheel angle
+var maxSpinTime := 0.5  ## the max amount of time you can spin the wheel
+var spinSpeedMultiplier := 0.07 ## the speed which the wheel spin will be multiplied by
+var power: float ## this is so when it needs to go flying, it uses the angle for power
+var initialPowerMultiplier := 1.7 ## just to give the spin an extra boost to make it feel better
+var flyDecay := 0.985 ## To know how fast it should stop spinning
+var flyExponential := 0.994 ## To make it slowdown exponentially 
+
+func _ready():
+	grabTimer.wait_time = maxSpinTime
+
+
+func _process(delta):
+	
+	
+	# ok, so we're going to track the mouse pos while it's in the area raidus
+	# the wheel will spin with the mouse from it's original position
+	# then when it leaves, we find the arc distance
+	# and use that for spin power
+	# this is about to be the least efficent code you've ever seen
+	if spinActive: # if you can spin
+		
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouseInArea: # if the player is holding the mouse down, and in the area
+			hasSpinBeenActivated = true
+			if !outOfTime: # if there is time left to drag
+				if firstClick == true: # if you just put the mouse down (to get inital pos)
+					firstClick = false
+					initialMousePos = get_local_mouse_position()
+				power = initialMousePos.angle_to(get_local_mouse_position()) * spinSpeedMultiplier
+				sprite.rotation += power
+				# theoretically, it should auto end the spinning if angle is PI/2, 
+				# but that's just unlikely with the shape of the donut, 
+				# so I won't bother accounting for it, and hope it doesn't bite me in the ass
+				
+			else: # when the time limit runs out
+				spinActive = false
+				flyActive = true
+				power *= initialPowerMultiplier
+				
+			if grabTimer.time_left == 0: # if the timer isn't going, start it
+				grabTimer.start()
+				outOfTime = false 
+		
+		else: # if no longer clicking or in area
+			if hasSpinBeenActivated: # only if it started to spin
+				spinActive = false
+				flyActive = true
+				hasSpinBeenActivated = false
+				power *= initialPowerMultiplier
+			grabTimer.stop() # end the timer to not cause issues
+			
+	else: # if you can't spin (to fix a couple things)
+		firstClick = true # to reset it for the next time
+		
+	
+	if flyActive:
+		sprite.rotation += power * flyDecay
+		flyDecay *= flyExponential
 	
 func _on_wheel_area_mouse_entered():
 	mouseInArea = true
